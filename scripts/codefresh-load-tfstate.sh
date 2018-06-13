4981a6627d602d13c437099f494fe05d71c455a4
Josh Dolitsky <jdolitsky@gmail.com>
2018-06-13 16:57:07 -0500

    fix issues with tfstate save/load

--- scripts/codefresh-load-tfstate.sh
+++ scripts/codefresh-load-tfstate.sh
@@ -5,7 +5,12 @@ cd $DIR/../
 
 set -o pipefail
 
+DECODE_FLAGS="-d"
+if [ "$(uname)" == "Darwin" ]; then
+    DECODE_FLAGS="-D"
+fi
+
 codefresh get context eks-install -o json | \
     jq -r '.spec.data.TFSTATE_BASE64' | \
     sed -e 's/^null$$//' | \
-    base64 -d > terraform/terraform.tfstate
+    base64 $DECODE_FLAGS > terraform/terraform.tfstate
--- scripts/codefresh-save-tfstate.sh
+++ scripts/codefresh-save-tfstate.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../
 
-export TFSTATE_BASE64="$(cat terraform/terraform.tfstate | base64)"
+export TFSTATE_BASE64="$(cat terraform/terraform.tfstate | base64 | tr -d '\n')"
 
 sed "s/TFSTATE_BASE64:.*/TFSTATE_BASE64: \"$TFSTATE_BASE64\"/g" eks-install-context-template.yaml \
     > eks-install-context.yaml
