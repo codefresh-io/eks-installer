0d2abf040244fa7db6c663a6516073943002211b
Josh Dolitsky <jdolitsky@gmail.com>
2018-06-13 23:29:56 -0500

    retry on terraform apply

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,7 +6,24 @@ cd $DIR/../
 cd terraform/
 
 terraform init
-terraform apply -auto-approve -var "cluster-name=${CLUSTER_NAME}" .
+
+# try 3 times in case we are stuck waiting for EKS cluster to come up
+set +e
+N=0
+SUCCESS="false"
+until [ $N -ge 3 ]; do
+  terraform apply -auto-approve -var "cluster-name=${CLUSTER_NAME}" .
+  if [[ "$?" == "0" ]]; then
+    SUCCESS="true"
+    break
+  fi
+  N=$[$N+1]
+done
+set -e
+
+if [[ "$SUCCESS" != "true" ]]; then
+    exit 1
+fi
 
 terraform output kubeconfig > ../kubeconfig.yaml
 terraform output config-map-aws-auth > ../config-map-aws-auth.yaml
