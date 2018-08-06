97e229fddd42ac087293c4b1083199019d2cd806
amir-codefresh <33693628+amir-codefresh@users.noreply.github.com>
2018-08-06 17:40:21 +0300

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,8 +4,8 @@ CLUSTER_NAME="${CLUSTER_NAME:-terraform-eks-demo}"
 CLUSTER_SIZE="${CLUSTER_SIZE:-1}"
 CLUSTER_REGION="${CLUSTER_REGION:-us-west-2}"
 CLUSTER_INSTANCE_TYPE="${CLUSTER_INSTANCE_TYPE:-m4.large}"
-KEY_PAIR_NAME="${KEY_PAIR_NAME:-default}"
-KEY_PAIR_PUBLIC_KEY="${CLUSTER_REGION:-default}"
+AWS_KEY_PAIR_NAME="${AWS_KEY_PAIR_NAME:-default}"
+AWS_SSH_PUBLIC_KEY="${AWS_SSH_PUBLIC_KEY:-default}"
 
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -22,8 +22,8 @@ SUCCESS="false"
 until [ $N -ge 3 ]; do
   terraform apply -auto-approve \
     -var "cluster-name=${CLUSTER_NAME}" \
-    -var "key-pair-name=${KEY_PAIR_NAME}" \
-    -var "key-pair-public-key=${KEY_PAIR_PUBLIC_KEY}" \
+    -var "key-pair-name=${AWS_KEY_PAIR_NAME}" \
+    -var "key-pair-public-key=${AWS_SSH_PUBLIC_KEY}" \
     -var "cluster-size=${CLUSTER_SIZE}" \
     -var "cluster-region=${CLUSTER_REGION}" \
     -var "cluster-instance-type=${CLUSTER_INSTANCE_TYPE}" \
