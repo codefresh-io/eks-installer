89d4004bcbbc9c0a38c6fcd19179bfa483aeb389
amir-codefresh <33693628+amir-codefresh@users.noreply.github.com>
2018-08-06 17:25:59 +0300

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,6 +4,9 @@ CLUSTER_NAME="${CLUSTER_NAME:-terraform-eks-demo}"
 CLUSTER_SIZE="${CLUSTER_SIZE:-1}"
 CLUSTER_REGION="${CLUSTER_REGION:-us-west-2}"
 CLUSTER_INSTANCE_TYPE="${CLUSTER_INSTANCE_TYPE:-m4.large}"
+KEY_PAIR_NAME="${KEY_PAIR_NAME:-default}"
+KEY_PAIR_PUBLIC_KEY="${CLUSTER_REGION:-default}"
+
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../
@@ -19,6 +22,8 @@ SUCCESS="false"
 until [ $N -ge 3 ]; do
   terraform apply -auto-approve \
     -var "cluster-name=${CLUSTER_NAME}" \
+    -var "key-pair-name=${KEY_PAIR_NAME}" \
+    -var "key-pair-public-key=${KEY_PAIR_PUBLIC_KEY}" \
     -var "cluster-size=${CLUSTER_SIZE}" \
     -var "cluster-region=${CLUSTER_REGION}" \
     -var "cluster-instance-type=${CLUSTER_INSTANCE_TYPE}" \
