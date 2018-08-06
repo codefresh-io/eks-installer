43041bf38bf481db0c4ede586a1a997eb5ce949f
amir-codefresh <33693628+amir-codefresh@users.noreply.github.com>
2018-08-06 17:20:20 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -21,3 +21,13 @@ variable "cluster-instance-type" {
   default = "m4.large"
   type    = "string"
 }
+
+variable "key-pair-name" {
+  default = "default"
+  type    = "string"
+}
+
+variable "key-pair-public-key" {
+  default = "default"
+  type    = "string"
+}
